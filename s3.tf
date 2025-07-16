4cf6c3064858239ffd30a63849419ac0a747cfa2
Anjali-Anamya <anjali.kr3086@gmail.com>
2025-07-16 11:24:12 +0530

    final s3

--- s3.tf
+++ s3.tf
@@ -3,3 +3,10 @@ resource "aws_s3_bucket" "artifact_store" {
   bucket = "anjali-artifact-store-bucket"
   force_destroy = true
 }
+resource "aws_s3_bucket_public_access_block" "artifact_store_block" {
+  bucket                  = aws_s3_bucket.artifact_store.id
+  block_public_acls       = true
+  ignore_public_acls      = true
+  block_public_policy     = true
+  restrict_public_buckets = true
+}
